9d015f42dc2c13f347ad3d546ab883932efb3191
schein00 <schein.jefer@gmail.com>
2016-04-15 17:44:30 -0300

    criacao do arquico para fazer lista duplamente encadeada

---
+++ lista.s
@@ -0,0 +1,33 @@
+# Trabalho Organizacao de computadores
+#
+#
+#
+			.data
+menu:   	.asciiz		
+					"\n\n-  Lista Ordenada  -\n-       MENU       -
+					\n1 - Incluir Elemento
+					\n2 - Excluir Elemento por indice
+					\n3 - Excluir Elemento por valor
+					\n4 - Mostrar Lista Ordenada
+					\n5 - Mostrar Totais					
+					\n6 - Sair					
+					\n\nopcao: "
+
+txt_invalida: 	.asciiz		"OPCAO INVALIDA\n"
+txt_valor: 		.asciiz		"Entre com o valor a ser inserido: "
+txt_indice: 	.asciiz		"Entre com o indice do valor a ser removido: "	
+txt_ind_inex:	.asciiz 	"Indice Inexistente\n"				
+txt_val_inex:	.asciiz 	"Valor Inexistente\n"	
+txt_valor_rem: 	.asciiz		"Entre com o valor a ser removido: "
+txt_vazia: 		.asciiz		"Lista Vazia \n"
+txt_sair: 		.asciiz		"SAIR \n"
+txt_total_inc: 	.asciiz		"\n\nTotal de inclusoes: "
+txt_total_exc:	.asciiz		"\nTotal de exclusoes: "
+txt_lista: 		.asciiz		"Lista: "
+txt_espaco: 	.asciiz		" "
+txt_posicao:		.asciiz     "Posicao Inserida: "
+txt_vremovido:	.asciiz		"Valor Removido: "
+txt_iremovido:	.asciiz		"Indice Removido: "
+		
+		.text
+main: 
